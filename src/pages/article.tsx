59c806f11160131a1d7bc88878d850cade534864
Victor Zhang <victormathwiz@gmail.com>
2023-02-15 01:15:40 -0500

    initial prototype/design

---
+++ src/pages/article.tsx
@@ -0,0 +1,54 @@
+
+export default function article(){
+    return (
+        <div className="article"> 
+            <p className="article--title">Letters from an American</p>
+            <p className="article--summary">Febuary 6, 2023</p>
+            <p className="article--body">
+                The Chinese spy balloon shot down off South Carolina on Saturday after spending four days in U.S. airspace will almost certainly make the history books but not because, by itself, it is a hugely significant factor in the changing relationship between the U.S. and China under President Joe Biden. The reason the balloon will be remembered in the future is that the Republican response to it has been so completely unrelated to reality, and has been so magnified by the media, that it has provided a window into the dysfunction of modern politics.
+                <br></br><br></br>
+                The facts are these: On Saturday, January 28, a Chinese airship entered U.S. airspace north of the Aleutian Islands, then crossed Alaska. It left U.S. airspace, then reentered over northern Idaho on Tuesday, January 31. On February 1 it was over Montana. On February 3 it was near St. Louis, Missouri. On Saturday, February 4, the pilot of an Air Force F-22 shot the airship down in shallow water off the coast of South Carolina, where the wreckage could be recovered.
+                <br></br><br></br>
+                The Trump administration had an inconsistent relationship with China. Trump attacked China in a trade war early in his presidency, placing tariffs on a range of products (which induced China to retaliate, prompting Trump to pump $28 billion into the U.S. farming sector to compensate for lost revenue). But by 2019, according to Trump’s national security advisor John Bolton, Trump “pleaded” with Chinese leader Xi Jinping to help him get reelected in 2020, and inked a deal for China to buy significant amounts of the farm products it had turned to other countries to provide after the tariffs (that was why Trump downplayed China’s role in hiding Covid in the early months of 2020). According to former representative Adam Kinzinger (R-IL), Trump also asked congressional leaders to “lay off” Xi, because Trump didn’t want to disappoint Xi.
+                <br></br><br></br>
+                In contrast, Biden and Secretary of State Antony Blinken have worked to counter China by building Indo-Pacific cooperation, reinforced U.S. support for Taiwan, established export controls on technology that have hamstrung the Chinese semiconductor industry, worked to counter Chinese investment in Africa, and enhanced security cooperation with South Korea and Japan.
+                <br></br><br></br>
+                But the balloon sparked a frenzy from Republicans insisting that Biden had been weak on China or even was working for China: right-wing talk show host Mark Levin said Biden is “bought and paid for by the Communist Chinese government,” and former president Trump said that Biden “has surrendered American airspace to Communist China.” Senator Marco Rubio (R-FL) said China was showing “that the United States is once-great superpower that’s hollowed out, it’s in decline.” South Carolina Republican representative Joe Wilson—the man who shouted “You lie” at President Obama—said that Biden and Vice President Kamala Harris should resign from office because of the balloon.
+                <br></br><br></br>
+                In fact, U.S. standing in the world has strengthened considerably since Biden took office, and the North Atlantic Treaty Organization (NATO), which Trump tried to scuttle, is strong enough that Sweden and Finland want to join.
+                <br></br><br></br>
+                It also turns out that at least three similar balloons crossed into U.S. airspace while Trump was president. Today, General Glen D. VanHerck, who oversees the North American Aerospace Defense Command, or NORAD, told reporters today that this weekend’s balloon was at least the fifth that had come into U.S. airspace, including at least three during Trump’s presidency, but that NORAD didn’t know about that until the intelligence community—under Biden—notified them.
+                <br></br><br></br>
+                As for the fact that Biden waited to shoot the thing down until it flew over the water, the administration says it did not want to take the risk of downing it over the American people. VanHerck estimated it weighed about 2,000 pounds, carried equipment the size of a regional jet, and was about 200 feet tall. As terrorism expert Malcolm Nance wrote on Twitter: “WHY let spy balloon in our space? 1) It was 18.5 miles up, almost in space. 2) it sends data link to PRC. We can intercept that & learn what China knows. We can jam it so they see nothing new. 3) The collection system is ours & can reconstruct it. They lose asset & we win spy game[.]” Indeed, U.S. officials say they blocked the instruments from gathering intelligence, and turned the tables to gather intelligence from the equipment itself.
+                <br></br><br></br>
+                You would think this balloon marks terrible U.S. weakness and is the most important thing to happen in years. But, in fact, the U.S. is stronger internationally than it has been in a while, and the balloon is just one more piece of a larger story about the changing relationship between China and the United States.
+                <br></br><br></br>
+                The breathless attention paid to the balloon starved a story that mattered far more in the long term: the economy under Biden has shown extraordinary job growth—another 517,000 jobs added in January—and the unemployment rate is at a low that has not been seen since 1969 (not a typo). Inflation is dropping. Today, Carly Wanna at Bloomberg noted that since the Inflation Reduction Act became law, more than 100,000 clean energy jobs have been created in the U.S. After months of reports that we are on the brink of a recession, today Treasury Secretary Janet Yellen said the chances of a recession are low. “You don’t have a recession when you have 500,000 jobs and the lowest unemployment rate in 50 years,” she said.
+                <br></br><br></br>
+                This economic news is not a blip; it is proof that Biden’s revival of the traditional understanding of how the economy works, shared by both parties before the 1980 election of Ronald Reagan, works.
+                <br></br><br></br>
+                Biden has rejected the trickle-down economics of the Republicans, which is based in the idea that moving capital upward will prompt investment in the economy and help everyone. In its place he has revived the older idea that investing in ordinary Americans and infrastructure creates widespread prosperity. His plan is a reversal of 40 years of economic policy, and we need to pay attention to it.
+                <br></br><br></br>
+                Biden has been crystal clear about the meaning behind his policies and has challenged House speaker Kevin McCarthy (R-CA) to lay out for the American people his own policies in a proposed annual budget. Instead, McCarthy is obfuscating, mixing together the debt ceiling, on which Biden refuses to negotiate because it is about funding obligations already incurred—in large part under Trump—and the budget, on which Biden has said he’s quite happy to negotiate.
+                <br></br><br></br>
+                McCarthy can’t produce a budget because his conference cannot agree on the cuts they insist are imperative. Instead, Republicans are threatening to refuse to lift the debt ceiling, although they lifted it three times under Trump. That refusal would tank the economy just before the 2024 election.
+                <br></br><br></br>
+                A poll conducted by the Washington Post and ABC News, written up today in the Washington Post, shows that 62% of Americans think Biden has not accomplished much in his two years in office. In fact, his administration ranks as one of the most consequential since the New Deal in the 1930s. Whether you love what he’s done or hate it, to think nothing has happened suggests a terrible disconnect between image and reality. Today at a press briefing, reporters peppered White House Director of the National Economic Council Brian Deese with questions about why that disconnect exists. Washington Post columnist Jennifer Rubin later tweeted, “Ummm. Heal thyself!”
+                <br></br><br></br>
+                Two other stories that are in the news today will likely also be remembered. Neo-Nazi leader Brandon Russell, 27, and Sarah Clendaniel, 34, were charged with plotting to bring down the electric power grid in Maryland, hoping to “destroy” Baltimore. In September, Ilana Krill and Bennett Clifford of the Program on Extremism at George Washington University warned that violent extremist white supremacists were focusing on attacking critical infrastructure “in the hopes that it will trigger a cataclysmic confrontation in American society and collapse the country from within.” And attacks on power stations have, indeed, been rising.
+                <br></br><br></br>
+                Finally, thousands are dead from the 7.8 magnitude earthquake and its strong aftershocks in Turkey and Syria last night. Biden has spoken to President Recep Tayyip Erdoğan of Turkey and pledged to support our NATO ally. U.S. teams will help search and rescue teams, as well as coordinate other assistance.
+            </p>
+
+            <h1>To-do list</h1>
+            <ul>
+                <li>fix navbar and add buttons? // figure out what top should like</li>
+                <li>format better within the margins</li>
+                <li>fix spacing of the article</li>
+                <li>building a form for responses on the bottom?</li>
+                <li>importing in fonts, should use nyt imperial?</li>
+                <li>thinking about how this works on phone</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/pages/navbar.tsx
@@ -0,0 +1,10 @@
+
+export default function NavBar(){
+    return (
+        <nav>
+            <img src="menu.png" className="nav--menu"/>
+            <p className="nav--title">Article Title</p>
+            <img src="help.png" className="nav--help"/>
+        </nav>
+    )
+}
