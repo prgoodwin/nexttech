8cba1013b3c32328cebb8e8c483e0876f3e5deaa
Victor Zhang <victormathwiz@gmail.com>
2023-03-29 09:55:34 -0400

    fixing rebase

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6419797f1ab6bda7"; }
+/******/ 		__webpack_require__.h = function() { return "47019f0805a091a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
